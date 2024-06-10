28a4af50aaffbcfab942dd2260c120b07ffdcd5e
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2024-06-10 15:56:40 +0600

    working on estate details page

--- src/pages/EstateDetails.jsx
+++ src/pages/EstateDetails.jsx
@@ -1,8 +1,14 @@
+import { useLoaderData, useParams } from "react-router-dom";
 import useDocumentTitle from "../hooks/useDocumentTitle";
 
 const EstateDetails = () => {
   // to do something more later
+  const data = useLoaderData();
+  const propertyId = useParams();
+  const currenEstate = data.filter((estate) => estate.id === propertyId);
   useDocumentTitle("Details ");
+  console.log(currenEstate);
+
   return <div>EstateDetails</div>;
 };
 
